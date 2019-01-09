a11262282090d744fb6b7b1e425391b76bb35c39
Sam Stevens <stevens.994@osu.edu>
2019-01-09 16:56:44 -0500

    added license

--- getFlight.js
+++ getFlight.js
@@ -1,3 +1,23 @@
+/*
+
+  Gets the seats, prices, and availability of WestJet flights.
+  Copyright (C) 2019 Samuel R Stevens
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 const longTime = 1000;
 const mediumTime = 500;
 const shortTime = 200;
